79072768d626cb0f5f33ac86e9bb035375044a0e
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-02-09 06:10:00 +0800

    2/9 check (object to array)

--- Trial/grade.py
+++ Trial/grade.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from pandas import json_normalize
 import json
-import numpy as np
 
-W = '\033[0m';Y = '\033[33m';B = '\033[34m'
+W = '\033[0m'
+Y = '\033[33m'
+B = '\033[34m'
 
-with open('output.json', 'r') as j:
+with open('Trial\output.json', 'r') as j:
     content = json.loads(j.read())
-    df = pd.json_normalize(content['calendarItems'])
-    df = df[['assignment.assignmentName', 'assignment.grade']]
-    for i in range(30, 34):
-        try:
-            print('Grade of ', B+df.iloc[i][0]+Y, int(df.iloc[i][1]))
-        except:
-            print(B+df.iloc[i][0]+Y, "No Grade Yet")
+    # print(content)
+df = pd.json_normalize(content)
+df = df[['assignment.assignmentName', 'assignment.grade']]
+for i in range(30, 34):
+    try:
+        print('Grade of ', B+df.iloc[i][0]+Y, int(df.iloc[i][1]))
+    except:
+        print(B+df.iloc[i][0]+Y, "No Grade Yet")
