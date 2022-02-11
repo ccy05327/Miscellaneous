1423442251ce663dd97d5019e7b218f163f5e0ee
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-02-11 12:08:12 +0800

    Update grade-1.py

--- Trial/grade-1.py
+++ Trial/grade-1.py
@@ -6,31 +6,35 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 import os
-
-CHROME_DRIVE_PATH = "/Users/ccy05327/Downloads/chromedriver"
-BINARY_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-USER_DATA_DIR = '/Users/ccy05327/Library/Application Support/Google/Chrome/Default'
-PROFILE_DIR = '/Users/ccy05327/Library/Application Support/Google/Chrome/Default/Profile 1'
+# Mac
+# CHROME_DRIVE_PATH = "/Users/ccy05327/Downloads/chromedriver"
+# BINARY_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+# USER_DATA_DIR = '/Users/ccy05327/Library/Application Support/Google/Chrome/Default'
+# PROFILE_DIR = '/Users/ccy05327/Library/Application Support/Google/Chrome/Default/Profile 1'
+# Windows
+CHROME_DRIVE_PATH = "D:\\Application\\chromedriver.exe"
+BINARY_PATH = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+USER_DATA_DIR = "C:\\Users\\hanna\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+PROFILE_DIR = 'C:\\Users\\hanna\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Profile 1'
 BSBC_HOME_TITLE = "Bachelor of Science in Computer Science | Home | Coursera"
-EMAIL = ""
-PASSWORD = ""
 
 login_url = "https://www.coursera.org/degrees/bachelor-of-science-computer-science-london?authMode=login"
 url = "https://www.coursera.org/degrees/bachelor-of-science-computer-science-london/home"
 
-s=Service(ChromeDriverManager().install())
+s = Service(ChromeDriverManager().install())
 options = Options()
 options.binary_location = BINARY_PATH
 options.add_argument("user-data-dir="+USER_DATA_DIR)
 options.add_argument("profile-directory="+PROFILE_DIR)
 
 driver = webdriver.Chrome(options=options, service=s)
-driver.set_window_position(0,0)
-driver.set_window_rect(1,1)
+driver.set_window_position(0, 0)
+driver.set_window_rect(1, 1)
+
 
 def parsePage():
     driver.minimize_window()
-    
+
     found_grade = False
 
     while found_grade == False:
@@ -43,7 +47,7 @@ def parsePage():
 
         time.sleep(4)
 
-        # find all of <a> tags 
+        # find all of <a> tags
         A_TAG_PATH = '//a[@data-click-key="degree_home.degree_home_page.click.item_link"]'
 
         a_tags = driver.find_elements_by_xpath(A_TAG_PATH)
@@ -63,13 +67,15 @@ def parsePage():
             for idx, txt in enumerate(a_tags):
                 content = txt.get_attribute("aria-label")
                 # time.sleep(1)
-                title_strings = txt.get_attribute("data-track-href").split("/")[2].split("-")
+                title_strings = txt.get_attribute(
+                    "data-track-href").split("/")[2].split("-")
                 title = " ".join(title_strings[1:])
                 if 'In Review' in content:
                     found_grade = True
                     print('#{} [{}]:\n {}\n-------------------------------------------'
-                        .format(idx+1, title.upper(), content))
+                          .format(idx+1, title.upper(), content))
         print("\n")
+
 
 def run():
     # time for you to be authorized
@@ -81,6 +87,7 @@ def run():
         parsePage()
 
     driver.close()
+
 
 if __name__ == "__main__":
     run()
