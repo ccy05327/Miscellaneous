a4cb35301e2f318823123ba59148b0cb45cfaa53
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-05-02 15:13:06 +0800

    declutter

--- Life/line_notify.py
+++ Life/line_notify.py
@@ -1,11 +1,12 @@
 from wsgiref import headers
 import requests
 
-LINE_TOKEN = 'HisNFhyDkaIvg9yfoBn3ZMMlCNKdqRknv506RnPxo0c'
+LINE_TO_ME_TOKEN = 'HisNFhyDkaIvg9yfoBn3ZMMlCNKdqRknv506RnPxo0c'
+# might have other tokens for other LINE groups
 URL = "https://notify-api.line.me/api/notify"
 
 headers = {
-    "Authorization": "Bearer " + LINE_TOKEN,
+    "Authorization": "Bearer " + LINE_TO_ME_TOKEN,
     "Content-Type": "application/x-www-form-urlencoded"
 }
 payload = {'message': "some test texts"}
--- TSV/video.py
+++ TSV/video.py
@@ -8,41 +8,41 @@ Y = '\033[33m'
 B = '\033[34m'
 P = '\033[35m'
 
-video_path = "/Volumes/explorationyear2020@gmail.com - Google Drive/My Drive/Personal/Image/TSV/TSYT/Regular/20220208 Dear Pop Song MVs, Please Stop/4s-eddy-bomb.mp4"
+video_path = "intro.mp4"
 
 # Read the video from specified path
 cam = cv2.VideoCapture(video_path)
 
 try:
-	# creating a folder named data
-	if not os.path.exists('data'):
-		os.makedirs('data')
+    # creating a folder named data
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
 # if not created then raise error
 except OSError:
-	print('Error: Creating directory of data')
+    print('Error: Creating directory of data')
 
 # frame
 currentframe = 0
 
 while(True):
-	# reading from frame
-	ret, frame = cam.read()
-	frameCount = 5
- 
-	if ret:
-		# if video is still left continue creating images
-		name = './data/eddy ' + str(int(currentframe/frameCount)) + '.jpg'
-		print('Creating ' + name)
+    # reading from frame
+    ret, frame = cam.read()
+    frameCount = 5
 
-		# writing the extracted images
-		cv2.imwrite(name, frame)
+    if ret:
+        # if video is still left continue creating images
+        name = './data/' + str(int(currentframe/frameCount)) + '.jpg'
+        print('Creating ' + name)
 
-		# increasing counter so that it will
-		# show how many frames are created
-		currentframe += frameCount
-	else:
-		break
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += frameCount
+    else:
+        break
 
 # Release all space and windows once done
 cam.release()
